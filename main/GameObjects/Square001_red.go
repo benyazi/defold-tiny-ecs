ca31115efda5f58fe7a113cf3cfdb81ba93e3727
Sergey Klabukov <benjamin.yazi@gmail.com>
2020-05-08 15:55:44 +0200

    Added pathfinding

---
+++ main/GameObjects/Square001_red.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite_square_001_red"
+  type: "sprite"
+  data: "tile_set: \"/Assets/background.atlas\"\n"
+  "default_animation: \"Square_001_red\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
